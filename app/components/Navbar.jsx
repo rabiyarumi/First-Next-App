9487a2382ee7d9abe89bef0eac794aec70db2e92
rabiyarumi <rabiyaarumi@gmail.com>
2025-01-24 22:05:40 +0600

    fix callback url

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,35 +6,41 @@ import {
   useKindeBrowserClient,
 } from "@kinde-oss/kinde-auth-nextjs";
 import Link from "next/link";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
-  const { user , isAuthenticated} = useKindeBrowserClient();
+  const { user, isAuthenticated } = useKindeBrowserClient();
+  const router = useRouter();
 
   const profileNavigation = () => {
-    if(!isAuthenticated){
-        redirect("/api/auth/login")
-      }
-      else{
-        redirect("/profile")
-      }
-  }
+    if (!isAuthenticated) {
+      router.push("/api/auth/login");
+    } else {
+      router.push("/profile");
+    }
+  };
+
+ 
 
   return (
     <nav className="flex justify-between items-center gap-6 py-8 w-[80%] mx-auto border-b-2">
       <div className="flex  gap-4 font-bold text-lg ">
         <Link href={"/"}>Home</Link>
-        <p onClick={profileNavigation} className="cursor-pointer">Profile</p>
+        <p onClick={profileNavigation} className="cursor-pointer">
+          Profile
+        </p>
       </div>
       <div>
-        { !!user ? (
+        {!!user ? (
           <LogoutLink className="font-bold text-black bg-white px-6 py-2 rounded-md hover:bg-white/70">
             Logout
           </LogoutLink>
         ) : (
-          <LoginLink className="font-bold text-black bg-white px-6 py-2 rounded-md hover:bg-white/70">
-            Login
-          </LoginLink>
+          <Link href="/api/auth/login">
+            <button className="font-bold text-black bg-white px-6 py-2 rounded-md hover:bg-white/70">
+              Login
+            </button>
+          </Link>
         )}
       </div>
     </nav>
